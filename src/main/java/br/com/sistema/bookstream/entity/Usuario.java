92022e0aae439d3decf45f739d61af0b1f90685b
César Augusto <cesar.augusto.rj1@gmail.com>
2026-02-23 21:41:45 -0300

    feat: Cria a entidade Usuario com atributos e anotações JPA

--- src/main/java/br/com/sistema/bookstream/entity/Usuario.java
+++ src/main/java/br/com/sistema/bookstream/entity/Usuario.java
@@ -1,5 +1,39 @@
 package br.com.sistema.bookstream.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios", uniqueConstraints = {@UniqueConstraint(name = "uq_login", columnNames = "login")})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "id_cliente", nullable = false)
+    private Long idCliente;
+
+    @Column(nullable = false, length = 100)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
